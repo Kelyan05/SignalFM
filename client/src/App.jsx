359d6e696e61eba2a1b7f8db1756f8cc0fadeef5
Kelyan <darrelkelyan@outlook.com>
2026-01-20 22:14:54 +0000

    Added protection to routes
    Only logged in uses can access the pages within SignalFM

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,61 @@
-import { useEffect, useState } from 'react'
-import Home from './pages/Home.jsx'
-import Login from './pages/Login.jsx'
-import Playlist from './pages/Playlist.jsx'
-import Discover from './pages/Discover.jsx'
-import {Routes, Route} from "react-router-dom"
-import NavBar from './components/NavBar.jsx'
-import Dashboard from './components/Dashboard.jsx'
-import { Auth } from './components/Auth.jsx'
-import { db } from './config/firebase'
-import { getDocs, collection, doc } from 'firebase/firestore'
-
-
+import { useEffect, useState } from "react";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Playlist from "./pages/Playlist.jsx";
+import Discover from "./pages/Discover.jsx";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar.jsx";
+import Dashboard from "./components/Dashboard.jsx";
+import { Auth } from "./components/Auth.jsx";
+import { auth } from "./config/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 function App() {
-  const [playlist,setPlaylist] = useState([])
+  const [user, setUser] = useState(null);
 
-  const playlistsCollectionRef = collection(db, "playlists")
-
-    //loads playlist from the firestore database
-    useEffect(() => {
-      const getPlaylist = async () => {
-        //reads data from firestore
-        // sets playlist state
-        try {
-          const data = await getDocs(playlistsCollectionRef)
-          const filteredData = data.docs.map((doc) => ({...doc.data(), id: doc.id}))
-          setPlaylist(filteredData) //to see the data in browser console
-        } catch (error) {
-          console.error("Error fetching playlists: ", error)
-      }
-      }
-
-      getPlaylist();
-    }, []);
+  useEffect(() => {
+    return onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  }, []);
 
   return (
     <div>
       <main className="main-content">
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/playlist" element={<Playlist />} />
-          <Route path="/discover" element={<Discover />} />
+          <Route path="/" element={<Login />} />
+
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute user={user}>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/playlist"
+            element={
+              <ProtectedRoute user={user}>
+                <Playlist />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/discover"
+            element={
+              <ProtectedRoute user={user}>
+                <Discover />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </main>
     </div>
-
   );
 }
 
